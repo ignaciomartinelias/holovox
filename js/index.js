50b3b9098ef2ad363c22a106cf973961d18eb5c0
Ignacio Martin Elias <ime@re-flow.io>
2020-02-19 16:09:29 +0100

    First Commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const edificio = document.querySelector(".bottom-section img");
+
+setTimeout(function(){
+    edificio.src = "../images/Building-1.png";
+}, 2750)
+
+setTimeout(function(){
+    edificio.src = "../images/Building-2.png";
+}, 3750)
+
+setTimeout(function(){
+    edificio.src = "../images/Building-3.png";
+}, 4750)
+
+setTimeout(function(){
+    edificio.src = "../images/Building-4.png";
+}, 5750)
+
+setTimeout(function(){
+    edificio.src = "../images/Building.svg";
+}, 6750)
