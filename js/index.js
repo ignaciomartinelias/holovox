0231abb83269289f1388193ba1c7e9e4f39f5dcf
Ignacio Martin Elias <ime@re-flow.io>
2020-02-19 17:27:41 +0100

    Fiz builidng 2

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 setTimeout(function(){
     document.getElementById("building-base").style.display = "none";
     document.getElementById("building-1").style.display = "block";
-}, 2900)
+}, 2750)
 
 setTimeout(function(){
         document.getElementById("building-1").style.display = "none";
     document.getElementById("building-2").style.display = "block";
-}, 3900)
+}, 3750)
 
 setTimeout(function(){
         document.getElementById("building-2").style.display = "none";
     document.getElementById("building-3").style.display = "block";
-}, 4900)
+}, 4750)
 
 setTimeout(function(){
         document.getElementById("building-3").style.display = "none";
     document.getElementById("building-4").style.display = "block";
-}, 5900)
+}, 5750)
 
 setTimeout(function(){
         document.getElementById("building-4").style.display = "none";
     document.getElementById("building").style.display = "block";
-}, 6900)
+}, 6750)
