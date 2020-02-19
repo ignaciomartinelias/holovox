162a57161631a114d61ae559c8bed597fdca1929
Ignacio Martin Elias <ime@re-flow.io>
2020-02-19 23:26:31 +0100

    Added pngs

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
-setTimeout(function(){
+setTimeout(function () {
+    console.log(document.getElementById("escombro-1").style.bottom);    
     document.getElementById("building-base").style.display = "none";
     document.getElementById("building-1").style.display = "block";
-}, 2750)
+}, 2000)
 
-setTimeout(function(){
-        document.getElementById("building-1").style.display = "none";
+setTimeout(function () {
+    document.getElementById("escombro-1").style.bottom = "60px";
+    document.getElementById("escombro-2").style.bottom = "50px";
+    document.getElementById("escombro-3").style.bottom = "60px";
+    document.getElementById("escombro-4").style.bottom = "50px";
+    document.getElementById("building-1").style.display = "none";
     document.getElementById("building-2").style.display = "block";
-}, 3750)
+}, 3000)
 
-setTimeout(function(){
-        document.getElementById("building-2").style.display = "none";
+setTimeout(function () {
+    document.getElementById("escombro-1").style.bottom = "80px";
+    document.getElementById("escombro-2").style.bottom = "70px";
+    document.getElementById("escombro-3").style.bottom = "80px";
+    document.getElementById("escombro-4").style.bottom = "70px";
+    document.getElementById("building-2").style.display = "none";
     document.getElementById("building-3").style.display = "block";
-}, 4750)
+}, 4000)
 
-setTimeout(function(){
-        document.getElementById("building-3").style.display = "none";
+setTimeout(function () {
+    document.getElementById("escombro-1").style.bottom = "100px";
+    document.getElementById("escombro-2").style.bottom = "90px";
+    document.getElementById("escombro-3").style.bottom = "100px";
+    document.getElementById("escombro-4").style.bottom = "90px";
+    document.getElementById("building-3").style.display = "none";
     document.getElementById("building-4").style.display = "block";
-}, 5750)
+}, 5000)
 
-setTimeout(function(){
-        document.getElementById("building-4").style.display = "none";
+setTimeout(function () {
+    document.getElementById("escombro-1").style.bottom = "120px";
+    document.getElementById("escombro-2").style.bottom = "110px";
+    document.getElementById("escombro-3").style.bottom = "120px";
+    document.getElementById("escombro-4").style.bottom = "110px";
+    document.getElementById("building-4").style.display = "none";
     document.getElementById("building").style.display = "block";
-}, 6750)
+}, 6000)
+
+setTimeout(function () {
+    document.getElementById("escombro-1").style.display = "none";
+    document.getElementById("escombro-2").style.display = "none";
+    document.getElementById("escombro-3").style.display = "none";
+    document.getElementById("escombro-4").style.display = "none";
+}, 7000)
